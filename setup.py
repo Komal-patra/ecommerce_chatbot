b3ea070a9429b06b9cc5bb576fdadc17c2da1e79
Komal-patra <komalfsds 2022@gmail.com>
2024-07-18 09:33:05 +0100

    project structure created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Ecommercebot",
+    version="0.0.1",
+    author="Komal",
+    author_email="komalfsds2022@gmail.com",
+    packages=find_packages(),
+    install_requires=['langchain-astradb','langchain ','langchain-openai','datasets','pypdf','python-dotenv','flask']
+)
